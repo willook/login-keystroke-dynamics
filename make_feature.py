50ec17e76f7db574fd36db0a6e41ca6dc986a86d
DoDaek <dbxor8960@naver.com>
2018-12-30 15:04:44 +0900

    수정파일

--- make_feature.py
+++ make_feature.py
@@ -1,44 +1,103 @@
 import numpy as np
-from keyboard import record
+from rss import rss
+from glob import glob
+from keyboard import record as recording
+from time import time
+from os import remove
 
-#if shift is pressed no reason
-#iamgod
+
+class KeyStroke():
+    file_path = None
+    tag_name = 'tag.txt'
+    n_patterns = 10
+    n_pattern = 10
+    n_current = 0
+
+    def __init__(self, file_path, n_patterns = 10, n_current = 0, tag_name = 'tag.txt'):
+        self.file_path = file_path
+        self.n_patterns = n_patterns
+        self.n_current = n_current
+        self.tag_name = tag_name
+
+    def _record_to_time(self, records):
+        names = []
+        times = []
+
+        t0 = records[0].time
+
+        # 마지막 인자는 엔터이므로 체크하지 않음
+        for i in range(len(records) - 1):
+            if records[i].event_type == 'down':
+                if records[i].name == 'space':
+                    records[i].name = ' '
+                if records[i].name == 'shift':
+                    if i != 0 and records[i - 1].name == 'shift':
+                        times = times[:-1]
+                        continue
+                    else:
+                        records[i].name = ''
+
+                if records[i].name == 'backspace':
+                    times = times[:-1]
+                    names = names[:-1]
+
+                if records[i].name == 'delete':
+                    times = times[:-1]
+                    names = names[:-1]
+
+                else:
+                    times.append(records[i].time - t0)
+                    names.append(records[i].name)
+        name = ''.join(names)
+        return times, name
+
+    def _recognition(self, X, y):
+        pass
+
+    def _typing_check(self, name)
+        f = open(self.file_path + self.tag_name)
+        line = f.readline()
+        return line == name
+
+    def _load_data(self):
+        patterns = glob(self.file_path + '*.npy')
+
+        self.n_current = len(patterns)
+        x_times = np.zeros((self.n_patterns, self.n_pattern))
+
+        for i in range(len(patterns)):
+            x_time = np.load(self.file_path + patterns[i])
+            x_times[i] = x_time
+        return x_times
+
+    def _update(self, time):
+        pass
+
+    def login(self):
+
+        while True:
+            records = recording(until='enter')
+            y_time, y_name = self._record_to_time(records)
+
+            # 입력이 태그와 같은지 확인
+            if self.typing_check(y_name):
+                break
+            else:
+                print("[!] 입력이 일치하지 않습니다")
+
+        # 참조 패턴을 불러옴
+        self.n_pattern = len(y_time)
+        x_times = self._load_data()
+
+        det = self._recognition(x_times, y_time):
+        if det:
+            print("login 성공")
+            self._update(y_time)
+
+        else det:
+            print("login 실패")
+
 
 if __name__ == '__main__':
-    names = ['./']
-    times = []
-
-    recorded = record(until='enter')
-    
-    t0 = recorded[0].time
-  
-    #except enter
-    for i in range(len(recorded)-1):
-        if recorded[i].event_type == 'down':
-            if recorded[i].name == 'space':
-                recorded[i].name = ' '
-            if recorded[i].name == 'shift':
-                if i != 0 and recorded[i-1].name == 'shift':
-                    times = times[:-1]
-                    continue
-                else:
-                    recorded[i].name = ''
-            
-            if recorded[i].name == 'backspace':
-                times = times[:-1]
-                names = names[:-1]
-                
-            if recorded[i].name == 'delete':
-                times = times[:-1]
-                names = names[:-1]
-                
-            else:
-                times.append(recorded[i].time-t0)
-                names.append(recorded[i].name)
-
-    name = ''.join(names)
-    print(name)
-    print(times)
-    print(len(times))
-    
-    np.save(name,times)
+    k1 = KeyStroke('Us/patterns/')
+    k1.login()
